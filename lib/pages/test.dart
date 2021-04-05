b3418d72b3847601c1b4732683bc9e32a1986f01
ambigo7 <dendirizkapoetra@gmail.com>
2021-04-05 16:51:54 +0700

    1.0.0

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -10,30 +10,8 @@ class _test_PagesState extends State<test_Pages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        elevation: 0.1,
-        backgroundColor: Colors.red,
-        title: InkWell(
-            onTap: () {
-//          ===Passing page no values with Navigator.push====
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => new HomePage()));
-            },
-            child: Text('Lets Shop')),
-        actions: <Widget>[
-          new IconButton(
-              icon: Icon(
-                Icons.search_outlined,
-                color: Colors.white,
-              ),
-              onPressed: () {}),
-          new IconButton(
-              icon: Icon(
-                Icons.shopping_cart_outlined,
-                color: Colors.white,
-              ),
-              onPressed: () {})
-        ],
+      body: new Center(
+        child: new Text('Error 404'),
       ),
     );
   }
