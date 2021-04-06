f4727666c7c242f0d068e999da210de1d67ec07c
ambigo7 <dendirizkapoetra@gmail.com>
2021-04-07 05:20:42 +0700

    v1.0.6(Sign In/Sing Up with Google bug fixed)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-//PACKAGE LINE ICONS FOR ICONS
-import 'package:line_icons/line_icons.dart';
-
-//PACKAGE BOTTOM BAR
-import 'package:google_nav_bar/google_nav_bar.dart';
+//FIREBASE CORE
+import 'package:firebase_core/firebase_core.dart';
 
 // MY OWN PACKAGE
 import 'package:lets_shop/pages/login.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
